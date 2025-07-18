b771da916cdfd15636ec20b3eb3eeb50adac7965
Jerryomwesh <jerryomwesh@gmail.com>
2025-07-18 21:29:18 +0300

    SBL-35: Add drag functionality to player tiles

--- vite-project/src/components/PlayerRack.jsx
+++ vite-project/src/components/PlayerRack.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import tileDistribution from '../data/tileDistribution';
 import '../styles/PlayerRack.css';
 
-const PlayerRack = () => {
+const PlayerRack = ({ onTileDragged }) => {
   const [playerTiles, setPlayerTiles] = useState([]);
   
   // Function to draw random tiles from the distribution
@@ -27,12 +27,49 @@ const PlayerRack = () => {
     setPlayerTiles(drawRandomTiles(7));
   }, []);
   
+  // Handle drag start event
+  const handleDragStart = (e, tile, index) => {
+    // Set the data being dragged
+    e.dataTransfer.setData('tileIndex', index);
+    e.dataTransfer.setData('tileLetter', tile.letter);
+    e.dataTransfer.setData('tilePoints', tile.points);
+    
+    // Add a class to style the tile being dragged
+    e.target.classList.add('dragging');
+    
+    // If parent component provided a callback, call it
+    if (onTileDragged) {
+      onTileDragged(tile, index);
+    }
+  };
+  
+  // Handle drag end event
+  const handleDragEnd = (e) => {
+    // Remove the dragging class
+    e.target.classList.remove('dragging');
+  };
+  
+  // Remove a tile from the rack (will be used later when implementing drop)
+  const removeTile = (index) => {
+    setPlayerTiles(prevTiles => {
+      const newTiles = [...prevTiles];
+      newTiles.splice(index, 1);
+      return newTiles;
+    });
+  };
+  
   return (
     <div className="player-rack-container">
       <h3>Your Tiles</h3>
       <div className="player-rack">
         {playerTiles.map((tile, index) => (
-          <div className="player-tile" key={index} draggable="true">
+          <div 
+            className="player-tile" 
+            key={index} 
+            draggable="true"
+            onDragStart={(e) => handleDragStart(e, tile, index)}
+            onDragEnd={handleDragEnd}
+          >
             <span className="tile-letter">{tile.letter}</span>
             <span className="tile-points">{tile.points}</span>
           </div>
